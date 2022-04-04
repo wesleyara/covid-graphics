6cd730f6c6931960062e7914464711ce2d11b110
wesleyara <wes1gom@gmail.com>
2022-04-04 12:09:11 -0300

    feat: Adicionando a API

---
+++ src/data/index.ts
@@ -0,0 +1,137 @@
+export const data = [
+  {
+    id: "0",
+    name: "Acre (AC)",
+    uf: "AC",
+  },
+  {
+    id: "1",
+    name: "Alagoas (AL)",
+    uf: "AL",
+  },
+  {
+    id: "2",
+    name: "Amapá (AP)",
+    uf: "AP",
+  },
+  {
+    id: "3",
+    name: "Amazonas (AM)",
+    uf: "AM",
+  },
+  {
+    id: "4",
+    name: "Bahia (BA)",
+    uf: "BA",
+  },
+  {
+    id: "5",
+    name: "Ceará (CE)",
+    uf: "CE",
+  },
+  {
+    id: "6",
+    name: "Distrito Federal (DF)",
+    uf: "DF",
+  },
+  {
+    id: "7",
+    name: "Espírito Santo (ES)",
+    uf: "ES",
+  },
+  {
+    id: "8",
+    name: "Goiás (GO)",
+    uf: "GO",
+  },
+  {
+    id: "9",
+    name: "Maranhão (MA)",
+    uf: "MA",
+  },
+  {
+    id: "10",
+    name: "Mato Grosso (MT)",
+    uf: "MT",
+  },
+  {
+    id: "11",
+    name: "Mato Grosso do Sul (MS)",
+    uf: "MS",
+  },
+  {
+    id: "12",
+    name: "Minas Gerais (MG)",
+    uf: "MG",
+  },
+  {
+    id: "13",
+    name: "Pará (PA)",
+    uf: "PA",
+  },
+  {
+    id: "14",
+    name: "Paraíba (PB)",
+    uf: "PB",
+  },
+  {
+    id: "15",
+    name: "Paraná (PR)",
+    uf: "PR",
+  },
+  {
+    id: "16",
+    name: "Pernambuco (PE)",
+    uf: "PE",
+  },
+  {
+    id: "17",
+    name: "Piauí (PI)",
+    uf: "PI",
+  },
+  {
+    id: "18",
+    name: "Rio de Janeiro (RJ)",
+    uf: "RJ",
+  },
+  {
+    id: "19",
+    name: "Rio Grande do Norte (RN)",
+    uf: "RN",
+  },
+  {
+    id: "20",
+    name: "Rio Grande do Sul (RS)",
+    uf: "RS",
+  },
+  {
+    id: "21",
+    name: "Rondônia (RO)",
+    uf: "RO",
+  },
+  {
+    id: "22",
+    name: "Roraima (RR)",
+    uf: "RR",
+  },
+  {
+    id: "23",
+    name: "Santa Catarina (SC)",
+    uf: "SC",
+  },
+  {
+    id: "24",
+    name: "São Paulo (SP)",
+    uf: "SP",
+  },
+  {
+    id: "25",
+    name: "Sergipe (SE)",
+    uf: "SE",
+  },
+  {
+    id: "26",
+    name: "Tocantins (TO)",
+    uf: "TO",
+  },
+];
