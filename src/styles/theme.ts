d946f0af2e361ba53012af365f2bf4dd425151ee
wesleyara <wes1gom@gmail.com>
2022-04-05 15:12:03 -0300

    style: Alterando estilos do theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,8 +3,8 @@ const theme = {
     background: "white",
     text: "#black",
     primary: "#8257e6",
-    headerBackground: "#C7C7C7",
-    hover: "#E6E6E6",
+    headerBackground: "#A2D5BE",
+    hover: "#469974",
   },
 };
 
